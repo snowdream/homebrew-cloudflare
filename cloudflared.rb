3fa74b6a22109049166b3f99a6f1b3845de5313e
yanghui <snowdream86@icloud.com>
2019-10-26 15:26:10 +0800

    Update cloudflared.rb

--- cloudflared.rb
+++ cloudflared.rb
@@ -1,7 +1,7 @@
 class Cloudflared < Formula
   desc 'Argo Tunnel'
   homepage 'https://developers.cloudflare.com/argo-tunnel/'
-  url 'https://developers.cloudflare.com/argo-tunnel/dl/cloudflared-2019.10.4-darwin-amd64.tgz'
+  url 'https://github.com/snowdream/homebrew-cloudflare/releases/download/2019.10.4/cloudflared-2019.10.4-darwin-amd64.tgz'
   sha256 '6e18796ea4e33080ef5e5bd9ad58fa5e37e0645f4f360660847053640de71b5a'
   version '2019.10.4'
   def install
